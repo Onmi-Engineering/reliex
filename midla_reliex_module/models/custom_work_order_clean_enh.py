ecfa014e5bbf2c726ba87b3106afbb67f21fa13b
matiasonmi <m.pretti@onmi.es>
2025-07-02 14:58:52 +0200

    [FIX] restrict email fields to type of cliente

--- midla_reliex_module/models/custom_work_order_clean_enh.py
+++ midla_reliex_module/models/custom_work_order_clean_enh.py
@@ -3,25 +3,22 @@ from odoo import fields, models, api, _
 class CustomWorkOrderClean(models.Model):
     _inherit = 'work.order.clean'
 
-    def _get_attendee_emails_reports2(self):
-        self.ensure_one()
-        emails = self.partner_id.certif_email
-        return emails
-
-    def _get_attendee_emails_confirmed2(self):
-        self.ensure_one()
-        emails = self.partner_id.stablish_email
-        return emails
-
-
     def _get_attendee_emails_reports(self):
         self.ensure_one()
         emails = self.partner_id.certif_email
         return emails
 
+    def _get_attendee_emails_reports2(self):
+        self.ensure_one()
+        emails = self.partner_id.certif_email
+        return emails
 
     def _get_attendee_emails_confirmed(self):
         """ Get comma-separated attendee email addresses from sending reports WOC. """
         self.ensure_one()
         return self.establishment_id.stablish_email or ""
 
+    def _get_attendee_emails_confirmed2(self):
+        self.ensure_one()
+        emails = self.partner_id.stablish_email
+        return emails
--- midla_reliex_module/models/res_partner_enh01.py
+++ midla_reliex_module/models/res_partner_enh01.py
@@ -3,7 +3,9 @@ from odoo import models, fields, api, _
 class RespartnerEnhancement(models.Model):
     _inherit = 'res.partner'
 
-    invoice_email = fields.Char(string="Email Factura")
-    certif_email = fields.Char(string="Email Certificado")
-    stablish_email = fields.Char(string="Email Establecimiento")
-
+    invoice_email = fields.Char(string="Email Factura",
+                                help="Este correo se utiliza para enviar la facturación al cliente.")
+    certif_email = fields.Char(string="Email Certificado",
+                               help="Este correo se utiliza para enviar certificados.")
+    stablish_email = fields.Char(string="Email Establecimiento",
+                                 help="Este correo se utiliza para enviar notificaciones de las OT a los establecimientos.")
