4366173b85b1dbe6dec7a05f6d33b7500b8629fd
matiasonmi <m.pretti@onmi.es>
2025-07-03 08:50:38 +0200

    FIX

--- midla_reliex_module/models/__init__.py
+++ midla_reliex_module/models/__init__.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
 
+from . import custom_account_move_send
+from . import custom_work_order_clean_enh
+from . import res_partner_enh01
 # from . import work_order_plant_enhancement01
 from . import worksheet_enhancement01
-from . import res_partner_enh01
-from . import custom_work_order_clean_enh
-from . import custom_account_move_send
+
+
--- midla_reliex_module/models/custom_work_order_clean_enh.py
+++ midla_reliex_module/models/custom_work_order_clean_enh.py
@@ -5,13 +5,14 @@ class CustomWorkOrderClean(models.Model):
 
     def _get_attendee_emails_reports(self):
         self.ensure_one()
-        emails = self.partner_id.certif_email
+        emails = self.establishment_id.certif_email
         return emails
 
     def _get_attendee_emails_reports2(self):
         self.ensure_one()
-        emails = self.partner_id.certif_email
+        emails = self.establishment_id.certif_email
         return emails
+
 
     def _get_attendee_emails_confirmed(self):
         """ Get comma-separated attendee email addresses from sending reports WOC. """
@@ -20,5 +21,6 @@ class CustomWorkOrderClean(models.Model):
 
     def _get_attendee_emails_confirmed2(self):
         self.ensure_one()
-        emails = self.partner_id.stablish_email
+        emails = self.establishment_id.stablish_email
         return emails
+
